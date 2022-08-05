6fa03488ee3dd1798994c64862ce24edd3bc36f1
kitae104 <aqua0405@gmail.com>
2022-08-05 17:21:42 +0900

    Thymeleaf 설정

--- src/main/java/kr/inhatc/spring/temp/controller/TempController.java
+++ src/main/java/kr/inhatc/spring/temp/controller/TempController.java
@@ -1,6 +1,7 @@
 package kr.inhatc.spring.temp.controller;
 
 import kr.inhatc.spring.temp.dto.TempDto;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,12 @@ public class TempController {
 
         return tempDto;
     }
+
+    // 반응 안함.. 컨트롤러 변경 필요!
+//    @GetMapping(value = "/ex01")
+//    public String ex01(Model model){
+//        model.addAttribute("data", "타임리프 예제 입니다.");
+//        return "temp/ex01";
+//    }
+
 }
