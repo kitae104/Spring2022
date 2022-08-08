160a58e7d74a073e9fa60bfe987b022598ea057d
Kitae's Github <aqua0405@gmail.com>
2022-08-08 10:57:46 +0900

    시큐리티 설정 시작

---
+++ src/main/java/kr/inhatc/spring/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package kr.inhatc.spring.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 인증 실패시 처리하는 클래스
+ * - 인증되지 않은 사용자의 리소스에 대한 접근처리 담당
+ */
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
---
+++ src/main/java/kr/inhatc/spring/constant/Role.java
@@ -0,0 +1,8 @@
+package kr.inhatc.spring.constant;
+
+/**
+ * 사용자 권한 설정
+ */
+public enum Role {
+    USER, ADMIN
+}
